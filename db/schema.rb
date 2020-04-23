a6ae7ee198168a7ff31a7b9eefee71429c50a69f
inoshun1118 <120561118@ccalumni.meijo-u.ac.jp>
2020-04-23 16:31:20 +0900

    push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_061522) do
+ActiveRecord::Schema.define(version: 2020_04_23_070234) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2020_04_23_061522) do
     t.string "address", null: false
     t.string "building_name"
     t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.integer "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
