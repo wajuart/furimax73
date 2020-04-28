ac7a9690f40c6e85898ec4cf22334ced602ba53f
inoshun1118 <120561118@ccalumni.meijo-u.ac.jp>
2020-04-28 13:53:34 +0900

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :products, only: [:index, :show]
-  root 'products#index'
+  root 'destinations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
