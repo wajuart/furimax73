120b293ec99c8518a9112ffea4ab5126f05121f9
ryo-hirano <62536923+ryo-hirano@users.noreply.github.com>
2020-04-29 09:22:02 +0900

    コメントアウト削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
   end
-
-  # def birth_day_join
-  #   # パラメータ取得
-  #   date = params[:user][:birth_day]
-
-  #   # ブランク時のエラー回避のため、ブランクだったら何もしない
-  #   if date["birth_day(1i)"].empty? && date["birth_day(2i)"].empty? && date["birth_day(3i)"].empty?
-  #     return
-  #   end
-
-  #   # 年月日別々できたものを結合して新しいDate型変数を作って返す
-  #   Date.new date["birth_day(1i)"].to_i,date["birth_day(2i)"].to_i,date["birth_day(3i)"].to_i
-  # end
 end
