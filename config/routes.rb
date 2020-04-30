e240c5fbe957795f44555bd469b8604ce3c294f2
wajuart <wajuart7@gmail.com>
2020-04-30 14:29:19 +0900

    [WIP]My page View

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :products, only: [:index, :show]
-  root 'users#edit'
+  root 'products#index'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
