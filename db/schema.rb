ae40a0ef66c3b4d71c16ee68991f14b8677cedd9
a-sai <62595316+a-sai@users.noreply.github.com>
2020-05-05 12:43:02 +0900

    fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -51,10 +51,11 @@ ActiveRecord::Schema.define(version: 2020_05_05_004407) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
-    t.integer "product_id", null: false
+    t.string "src", null: false
+    t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -85,16 +86,17 @@ ActiveRecord::Schema.define(version: 2020_05_05_004407) do
     t.datetime "updated_at", null: false
     t.string "user_image"
     t.text "introduction"
-    t.string "family_name"
-    t.string "first_name"
-    t.string "family_name_kana"
-    t.string "first_name_kana"
+    t.string "family_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
     t.string "nickname", default: "", null: false
-    t.date "birth_day"
+    t.date "birth_day", null: false
     t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "products"
 end
