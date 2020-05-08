46aafc228084966af8b114246f32a5f2302be873
a-sai <62595316+a-sai@users.noreply.github.com>
2020-05-08 15:59:15 +0900

    add sale function
    出品機能の実装
    カテゴリー機能実装
    JSの実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,22 +6,45 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @images = @product.images.build
     @product.images.new
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+    end
+  end
+
+  def get_category_children
+    # binding.pry
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+ # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
   def show
   end
 
   def create
+    binding.pry
     @product = Product.new(product_params)
     if @product.save
       redirect_to root_path
     else
+      @category_parent_array = ["---"]
+      Category.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+    end
       render :new
     end
   end
 
-  before_action :set_product, except: [:index, :new, :create, :show]
+  def edit
+  end
 
   def update
     if @product.update(product_params)
@@ -31,20 +54,15 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :description, :status, :size, :shipping_cost, :shipping_days, :prefecture_id, :judgment, :category_id, :brand_id, :shipping_id, :user_id, images_attributes: [:src, :_destroy, :id])
-  end
-
-  def set_product
-    @product = Product.find(params[:id])
-  end
-
-  def edit
-  end
-
-  def destroy
+    params.require(:product).permit(:name, :price, :description, :status, :size, :shipping_cost, :shipping_days, :prefecture_id, :judgment, :category_id, :brand_id, :shipping_id, images_attributes: [:src, :_destroy, :id]).merge(user_id: current_user.id)
   end
 
 end
