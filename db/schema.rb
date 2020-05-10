9a168175267a44e4613f8180661dbd6a34493048
inoshun1118 <120561118@ccalumni.meijo-u.ac.jp>
2020-05-10 22:32:07 +0900

    push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_081925) do
+ActiveRecord::Schema.define(version: 2020_05_05_004407) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -94,6 +94,8 @@ ActiveRecord::Schema.define(version: 2020_04_28_081925) do
     t.string "first_name_kana", default: "", null: false
     t.string "nickname", default: "", null: false
     t.date "birth_day", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
