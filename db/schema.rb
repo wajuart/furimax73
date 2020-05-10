5d325cd39e07d9fddfdde443c2936de4d0854d9d
inoshun1118 <120561118@ccalumni.meijo-u.ac.jp>
2020-05-10 23:00:30 +0900

    push

--- db/schema.rb
+++ db/schema.rb
@@ -94,8 +94,6 @@ ActiveRecord::Schema.define(version: 2020_04_28_081925) do
     t.string "first_name_kana", default: "", null: false
     t.string "nickname", default: "", null: false
     t.date "birth_day", null: false
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
