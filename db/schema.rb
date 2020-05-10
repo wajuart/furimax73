3d70cde1742a92d9c38f78687426dbd1a7d44916
inoshun1118 <120561118@ccalumni.meijo-u.ac.jp>
2020-05-10 22:54:58 +0900

    push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_004407) do
+ActiveRecord::Schema.define(version: 2020_04_28_081925) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
