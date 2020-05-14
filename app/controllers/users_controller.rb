fdc74b844f68617aaae32dff53545e95e2b9eba2
wajuart <wajuart7@gmail.com>
2020-05-14 12:27:51 +0900

    ユーザー編集機能調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,15 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end  
 
 end
