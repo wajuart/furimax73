2aa6cb1743b8e5912a1c47a0ea8bb5f328d6d556
wajuart <wajuart7@gmail.com>
2020-05-15 21:28:36 +0900

    各種調整

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,7 @@ class ProductsController < ApplicationController
 
   def new
     unless user_signed_in?
+      flash[:alert] = "ログインしてください"
       redirect_to root_path
     end
     @product = Product.new
