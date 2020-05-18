a335eea26b9a428e44f31239ccd9378c5e094669
wajuart <wajuart7@gmail.com>
2020-05-18 11:06:39 +0900

    マイページ関連修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
 
   def index
     @products = current_user.products
-    # user = User.find(params[:id])
-    # user = User.includes(:products)
-    # @products = Product.find(params[:id])
-    # @nickname = user.nickname
   end
   
   def new
@@ -16,8 +12,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    # User.create(user_params)
-    @user = User.new(user_params)
   end  
 
   def show
@@ -29,23 +23,18 @@ class UsersController < ApplicationController
   end
   
   def update
-    # binding.pry
     @user = User.find(params[:id])
     if current_user.update(user_params)
-      flash[:success] = 'ユーザー情報を編集しました。'
       redirect_to user_path(current_user)
     else
-      flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
       render :edit
     end
   end
-
-
+  
   private
   
   def user_params
     params.require(:user).permit(:user_image, :nickname, :introduction)
   end
-
 
 end
